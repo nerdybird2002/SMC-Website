5c9dae4fbc30c1a6b28070fdd29ff80ba9a0a3b5
nerdybird2002 <coolsarahs21@gmail.com>
2025-10-18 23:20:24 -0400

    header code working

---
+++ js/header.js
@@ -0,0 +1,18 @@
+header.innerHTML = `
+<header class="my-header">
+<div>
+<a href="index.html" target="_self" title="home">
+          <button>Home</button>
+        </a>
+        <a href="about.html" target="_self" title="about">
+          <button>About</button>
+        </a>
+        <a href="events.html" target="_self" title="events">
+          <button>Events</button>
+        </a>
+        <a href="Resources.html" target="_self" title="resources">
+          <button>Resources</button>
+        </a>
+        </div>
+        </header>
+        `;
